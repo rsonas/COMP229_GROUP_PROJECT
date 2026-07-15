2b357d92e30abf4c31c90190a73cb6492bc38c94
rsonas <134102387+rsonas@users.noreply.github.com>
2026-07-15 17:13:28 -0400

    db connection
    established connection to database

--- backend/express.js
+++ backend/express.js
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 
 const app = express();
 
+//setting up middleware
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-import app from './backend/express.js';
+import 'dotenv/config'
+import app from './backend/express.js'
+import config from './backend/config/config.js'
+import mongoose from 'mongoose'
 
+//connect to db
+mongoose.connect(config.mongoUri)
+    .then(() => console.log("Connected to database"))
+
+mongoose.connection.on('error', () => {
+    throw new Error(`Unable to connect to database: ${config.mongoUri}`);
+})
+
+//launch server
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
