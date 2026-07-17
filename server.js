4dee7b909d30e151cc0e38748cde21ee64894019
rsonas <134102387+rsonas@users.noreply.github.com>
2026-07-17 18:16:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -17,3 +17,9 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Backend launched successfully!"
+    });
+});
