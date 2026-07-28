b281edc31a57988284c668ebc168d9a04fb60dbc
fdebugging <fatima_dabbous@hotmail.com>
2026-07-28 17:12:07 -0300

    Set up Axios API configuration for authentication

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,1 +1,22 @@
 //(connect frontend to backend)
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "http://localhost:3000",
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+// Automatically attach JWT token to every request
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
+export default api;
