a67b546e54804b7f186a8fb90e219438c90d3845
rsonas <134102387+rsonas@users.noreply.github.com>
2026-08-13 00:20:32 -0400

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -15,7 +15,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+    baseURL: "https://comp229-group-project-backend-u8eg.onrender.com/",
     headers: {
         "Content-Type": "application/json",
     },
